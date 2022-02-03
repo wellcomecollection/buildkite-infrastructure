98a2e7b97d04962a1b6a3148060c42a07fb687ac
Alex Chan <a.chan@wellcome.org>
2022-02-03 15:34:56 +0000

    Set up logging on the secrets bucket
    It's provided by Buildkite, we might as well use it.

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -5,6 +5,15 @@ locals {
 resource "aws_s3_bucket" "buildkite_secrets" {
   bucket = "wellcomecollection-buildkite-secrets"
   acl    = "private"
+
+  logging {
+    target_bucket = aws_s3_bucket.buildkite_secrets_logging.id
+  }
+}
+
+resource "aws_s3_bucket" "buildkite_secrets_logging" {
+  bucket = "wellcomecollection-buildkite-secrets-logging"
+  acl    = "private"
 }
 
 resource "aws_s3_bucket" "buildkite_artifacts" {
