453d4c7534462112fb885fffd90a318e9bca143e
Alex Chan <a.chan@wellcome.org>
2022-02-03 14:56:46 +0000

    Inline the VPC endpoint definitions

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,25 +1,45 @@
-module "ecr_dkr_vpc_endpoint" {
-  source = "../../accounts/modules/interface_endpoint"
-
+data "aws_vpc_endpoint_service" "ecr_dkr" {
   service = "ecr.dkr"
-  vpc_id  = local.ci_vpc_id
-
-  security_group_ids = [
-    aws_security_group.buildkite.id,
-  ]
-
-  subnet_ids = local.ci_vpc_private_subnets
 }
 
-module "ecr_api_vpc_endpoint" {
-  source = "../../accounts/modules/interface_endpoint"
-
-  service = "ecr.api"
-  vpc_id  = local.ci_vpc_id
+resource "aws_vpc_endpoint" "ecr_dkr" {
+  vpc_id            = local.ci_vpc_id
+  vpc_endpoint_type = "Interface"
 
   security_group_ids = [
     aws_security_group.buildkite.id,
   ]
 
   subnet_ids = local.ci_vpc_private_subnets
+
+  service_name = data.aws_vpc_endpoint_service.ecr_dkr.service_name
+
+  private_dns_enabled = true
+
+  tags = {
+    Name = "buildkite-ecr_dkr-vpc_endpoint"
+  }
+}
+
+data "aws_vpc_endpoint_service" "ecr_api" {
+  service = "ecr.api"
+}
+
+resource "aws_vpc_endpoint" "ecr_api" {
+  vpc_id            = local.ci_vpc_id
+  vpc_endpoint_type = "Interface"
+
+  security_group_ids = [
+    aws_security_group.buildkite.id,
+  ]
+
+  subnet_ids = local.ci_vpc_private_subnets
+
+  service_name = data.aws_vpc_endpoint_service.ecr_api.service_name
+
+  private_dns_enabled = true
+
+  tags = {
+    Name = "buildkite-ecr_api-vpc_endpoint"
+  }
 }
